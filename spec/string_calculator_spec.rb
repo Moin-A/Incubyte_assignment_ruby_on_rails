faaed833a734e44ce08980b7a2575ca3cf2d2dc2
Moin-A <moinqa6@gmail.com>
2025-08-04 13:04:38 +0530

    RED: Add failing test for single number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,5 +6,11 @@ describe StringCalculator do
     it 'returns 0 for empty string' do
       expect(StringCalculator.add("")).to eq(0)
     end
+
+    it 'returns the number itself for single number' do
+        expect(StringCalculator.add("1")).to eq(1)
+        expect(StringCalculator.add("5")).to eq(5)
+    end
+
   end
 end
